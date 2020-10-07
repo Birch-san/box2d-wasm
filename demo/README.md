ea5b5823477c52481e7db09d0cd994dbfb203bbd
Alex Birch <Birch-san@users.noreply.github.com>
2020-10-07 22:44:12 +0100

    delete the non-working snowpack and webpack demos

--- demo/README.md
+++ demo/README.md
@@ -1,29 +1,105 @@
-# New Project
+*Looking for a shareable component template? Go here --> [sveltejs/component-template](https://github.com/sveltejs/component-template)*
 
-> ✨ Bootstrapped with Create Snowpack App (CSA).
+---
 
-## Available Scripts
+# svelte app
 
-### npm start
+This is a project template for [Svelte](https://svelte.dev) apps. It lives at https://github.com/sveltejs/template.
 
-Runs the app in the development mode.
-Open http://localhost:8080 to view it in the browser.
+To create a new project based on this template using [degit](https://github.com/Rich-Harris/degit):
 
-The page will reload if you make edits.
-You will also see any lint errors in the console.
+```bash
+npx degit sveltejs/template svelte-app
+cd svelte-app
+```
 
-### npm test
+*Note that you will need to have [Node.js](https://nodejs.org) installed.*
 
-Launches the test runner in the interactive watch mode.
-See the section about running tests for more information.
 
-### npm run build
+## Get started
 
-Builds a static copy of your site to the `build/` folder.
-Your app is ready to be deployed!
+Install the dependencies...
 
-**For the best production performance:** Add a build bundler plugin like "@snowpack/plugin-webpack" or "@snowpack/plugin-parcel" to your `snowpack.config.json` config file.
+```bash
+cd svelte-app
+npm install
+```
 
-### Q: What about Eject?
+...then start [Rollup](https://rollupjs.org):
 
-No eject needed! Snowpack guarantees zero lock-in, and CSA strives for the same.
+```bash
+npm run dev
+```
+
+Navigate to [localhost:5000](http://localhost:5000). You should see your app running. Edit a component file in `src`, save it, and reload the page to see your changes.
+
+By default, the server will only respond to requests from localhost. To allow connections from other computers, edit the `sirv` commands in package.json to include the option `--host 0.0.0.0`.
+
+If you're using [Visual Studio Code](https://code.visualstudio.com/) we recommend installing the official extension [Svelte for VS Code](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode). If you are using other editors you may need to install a plugin in order to get syntax highlighting and intellisense.
+
+## Building and running in production mode
+
+To create an optimised version of the app:
+
+```bash
+npm run build
+```
+
+You can run the newly built app with `npm run start`. This uses [sirv](https://github.com/lukeed/sirv), which is included in your package.json's `dependencies` so that the app will work when you deploy to platforms like [Heroku](https://heroku.com).
+
+
+## Single-page app mode
+
+By default, sirv will only respond to requests that match files in `public`. This is to maximise compatibility with static fileservers, allowing you to deploy your app anywhere.
+
+If you're building a single-page app (SPA) with multiple routes, sirv needs to be able to respond to requests for *any* path. You can make it so by editing the `"start"` command in package.json:
+
+```js
+"start": "sirv public --single"
+```
+
+## Using TypeScript
+
+This template comes with a script to set up a TypeScript development environment, you can run it immediately after cloning the template with:
+
+```bash
+node scripts/setupTypeScript.js
+```
+
+Or remove the script via:
+
+```bash
+rm scripts/setupTypeScript.js
+```
+
+## Deploying to the web
+
+### With [Vercel](https://vercel.com)
+
+Install `vercel` if you haven't already:
+
+```bash
+npm install -g vercel
+```
+
+Then, from within your project folder:
+
+```bash
+cd public
+vercel deploy --name my-project
+```
+
+### With [surge](https://surge.sh/)
+
+Install `surge` if you haven't already:
+
+```bash
+npm install -g surge
+```
+
+Then, from within your project folder:
+
+```bash
+npm run build
+surge public my-project.surge.sh
+```
--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,60 +1,181 @@
 <script lang="typescript">
-  import * as Box2D from 'Box2D';
+  import Box2D from 'Box2D';
   import { onMount } from 'svelte';
+  import { CanvasDebugDraw } from './debugDraw';
+  import { Helpers } from './helpers';
 
-	let canvas: HTMLCanvasElement;
+  let canvas: HTMLCanvasElement;
+  
+  interface Point {
+    x: number;
+    y: number;
+  }
 
 	onMount(async () => {
-    console.log(Box2D);
-    debugger;
     const box2D = await Box2D();
-    // const bd_ground = new b2BodyDef();
-    // const groundBody = world.CreateBody(bd_ground);
 
-		const ctx = canvas.getContext('2d');
-		let handle: number | undefined;
+    const ctx = canvas.getContext('2d');
+    const canvasOffset: Point = {
+      x: canvas.width/2,
+      y: canvas.height/2
+    };
+    const viewCenterPixel: Point = {
+      x: canvas.width/2,
+      y: canvas.height/2
+    };
 
-		(function loop() {
-			handle = requestAnimationFrame(loop);
+    const { b2_dynamicBody, b2BodyDef, b2CircleShape, b2Draw: { e_shapeBit }, b2EdgeShape, b2Fixture,
+      b2Vec2, b2World, destroy, JSQueryCallback, wrapPointer } = box2D;
+    const myQueryCallback = new JSQueryCallback();
 
-			const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
+    myQueryCallback.ReportFixture = (fixturePtr: any) => {
+        const fixture = wrapPointer( fixturePtr, b2Fixture );
+        if ( fixture.GetBody().GetType() != b2_dynamicBody ) //mouse cannot drag static bodies around
+          return true;
+        console.log(fixture);
+        return false;
+    };
+    const helpers = new Helpers(box2D);
+    const { createPolygonShape, createRandomPolygonShape, createChainShape } = helpers;
+    const debugDraw = new CanvasDebugDraw(box2D, helpers, ctx!).constructJSDraw();
+    debugDraw.SetFlags(e_shapeBit);
+    const world = new b2World(new b2Vec2(0.0, -10.0));
+    world.SetDebugDraw(debugDraw);
+    const bd_ground = new b2BodyDef();
+    const groundBody = world.CreateBody(bd_ground);
 
-			for (let p = 0; p < imageData.data.length; p += 4) {
-				const i = p / 4;
-				const x = i % canvas.width;
-				const y = i / canvas.height >>> 0;
+    //ground edges
+    const shape0 = new b2EdgeShape();
+    shape0.SetTwoSided(new b2Vec2(-40.0, -6.0), new b2Vec2(40.0, -6.0));
+    groundBody.CreateFixture(shape0, 0.0);
+    shape0.SetTwoSided(new b2Vec2(-9.0, -6.0), new b2Vec2(-9.0, -4.0));
+    groundBody.CreateFixture(shape0, 0.0);
+    shape0.SetTwoSided(new b2Vec2(9.0, -6.0), new b2Vec2(9.0, -4.0));
+    groundBody.CreateFixture(shape0, 0.0);
 
-				const t = window.performance.now();
+    const cshape = new b2CircleShape();
+    cshape.set_m_radius(0.5);
 
-				const r = 64 + (128 * x / canvas.width) + (64 * Math.sin(t / 1000));
-				const g = 64 + (128 * y / canvas.height) + (64 * Math.cos(t / 1000));
-				const b = 128;
+    //falling shapes
+    const ZERO = new b2Vec2(0, 0);
+    const temp = new b2Vec2(0, 0);
+    new Array(20).fill(undefined).forEach((_, index: number) => {
+      const bd = new b2BodyDef();
+      bd.set_type(b2_dynamicBody);
+      bd.set_position(ZERO);
+      const body = world.CreateBody(bd);
+      const randomValue = Math.random();
+      const shape = randomValue < 0.2 ? cshape : createRandomPolygonShape(0.5);
+      body.CreateFixture(shape, 1.0);
+      temp.Set(16*(Math.random()-0.5), 4.0 + 2.5*index);
+      body.SetTransform(temp, 0.0);
+      body.SetLinearVelocity(ZERO);
+      body.SetAwake(1);
+      // body.SetActive(1); // no longer exists
+    });
 
-				imageData.data[p + 0] = r;
-				imageData.data[p + 1] = g;
-				imageData.data[p + 2] = b;
-				imageData.data[p + 3] = 255;
-			}
+    //static polygon and chain shapes
+    {
+      const verts = [];
+      verts.push( new b2Vec2( 7,-1) );
+      verts.push( new b2Vec2( 8,-2) );
+      verts.push( new b2Vec2( 9, 3) );
+      verts.push( new b2Vec2( 7, 1) );
+      const polygonShape = createPolygonShape(verts);
+      groundBody.CreateFixture(polygonShape, 0.0);
+      
+      //mirror vertices in x-axis and use for chain shape
+      for (let i = 0; i < verts.length; i++)
+          verts[i].set_x( -verts[i].get_x() );
+      verts.reverse();
+      const chainShape = createChainShape(verts, true);//true for closed loop *** some problem with this atm
+      // polygonShape = createPolygonShape(verts);
+      groundBody.CreateFixture(chainShape, 0.0);
+    }
 
-			ctx!.putImageData(imageData, 0, 0);
-		}());
+    const PTM = 32;
+
+    const myRound = (val: number, places: number) => {
+        let c = 1;
+        for (let i = 0; i < places; i++)
+          c *= 10;
+        return Math.round(val*c)/c;
+    }
+
+    const getWorldPointFromPixelPoint = (pixelPoint: Point) => ({
+      x: (pixelPoint.x - canvasOffset.x)/PTM,
+      y: (pixelPoint.y - (canvas.height - canvasOffset.y))/PTM
+    });
+
+    const setViewCenterWorld = (b2vecpos: any, instantaneous: any): void => {
+      var currentViewCenterWorld = getWorldPointFromPixelPoint( viewCenterPixel );
+      var toMoveX = b2vecpos.get_x() - currentViewCenterWorld.x;
+      var toMoveY = b2vecpos.get_y() - currentViewCenterWorld.y;
+      var fraction = instantaneous ? 1 : 0.25;
+      canvasOffset.x -= myRound(fraction * toMoveX * PTM, 0);
+      canvasOffset.y += myRound(fraction * toMoveY * PTM, 0);
+    };
+    setViewCenterWorld( new b2Vec2(0,0), true );
+
+
+    const draw = () => {
+      //black background
+      ctx!.fillStyle = 'rgb(0,0,0)';
+      ctx!.fillRect( 0, 0, canvas.width, canvas.height );
+
+      ctx!.save();
+      ctx!.translate(canvasOffset.x, canvasOffset.y);
+      ctx!.scale(1,-1);                
+      ctx!.scale(PTM, PTM);
+      ctx!.lineWidth /= PTM;
+      
+      CanvasDebugDraw.drawAxes(ctx!);
+      
+      ctx!.fillStyle = 'rgb(255,255,0)';
+      world.DebugDraw();
+      
+      // if ( mouseJoint != null ) {
+      //     //mouse joint is not drawn with regular joints in debug draw
+      //     var p1 = mouseJoint.GetAnchorB();
+      //     var p2 = mouseJoint.GetTarget();
+      //     ctx!.strokeStyle = 'rgb(204,204,204)';
+      //     ctx!.beginPath();
+      //     ctx!.moveTo(p1.get_x(),p1.get_y());
+      //     ctx!.lineTo(p2.get_x(),p2.get_y());
+      //     ctx!.stroke();
+      // }
+          
+      ctx!.restore();
+    };
+
+    let handle: number | undefined;
+
+    (function loop(prevMs: number) {
+      const nowMs = window.performance.now();
+      handle = requestAnimationFrame(loop.bind(null, nowMs));
+      const delta = nowMs-prevMs;
+
+			world.Step(delta/1000, 3, 2);
+      draw();
+		}(window.performance.now()));
 
 		return () => {
-			cancelAnimationFrame(handle!);
+      cancelAnimationFrame(handle!);
+      destroy(world);
 		};
 	});
 </script>
 
 <style>
   canvas {
-    background-color: #666;
-    -webkit-mask: url(logo.svg) 50% 50% no-repeat;
-    mask: url(logo.svg) 50% 50% no-repeat;
+    background-color: #ddd;
+    /* -webkit-mask: url(logo.svg) 50% 50% no-repeat;
+    mask: url(logo.svg) 50% 50% no-repeat; */
   }
 </style>
 
 <canvas
 	bind:this={canvas}
-	width={1280}
-	height={720}
+	width={640}
+	height={480}
 ></canvas>
