8dd49b856c811660cdcac97f02c0a822225daa80
Alex Birch <Birch-san@users.noreply.github.com>
2020-10-07 18:58:23 +0100

    add TS linting, probably. and upgrade TS to 4.0.0

---
+++ demo3/.eslintrc.js
@@ -0,0 +1,37 @@
+const eslintSveltePreprocess = require('eslint-svelte3-preprocess');
+const svelteConfig = require('./svelte.config');
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  env: {
+    es6: true,
+    browser: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  plugins: [
+    'svelte3',
+    '@typescript-eslint',
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+		{
+			files: ["*.ts"],
+			extends: [
+				"plugin:@typescript-eslint/recommended"
+			],
+		},
+  ],
+  settings: {
+    'svelte3/preprocess': eslintSveltePreprocess(svelteConfig.preprocess),
+	},
+};
