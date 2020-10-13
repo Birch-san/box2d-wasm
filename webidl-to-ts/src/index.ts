d9373cbed8fa71b9e59cee2197504212f3d962ba
Alex Birch <Birch-san@users.noreply.github.com>
2020-10-13 22:48:57 +0100

    no-op transformer

--- webidl-to-ts/src/index.ts
+++ webidl-to-ts/src/index.ts
@@ -1,4 +1,4 @@
-import ts from 'typescript';
+import ts, { SourceFileLike } from 'typescript';
 import yargs from 'yargs';
 import path from 'path';
 import fs from 'fs';
@@ -15,10 +15,17 @@ const argv = yargs(process.argv.slice(2))
     nargs: 1,
     describe: 'Load a file'
   })
+  .option('o', {
+    type: 'string',
+    demandOption: false,
+    alias: 'file',
+    nargs: 1,
+    describe: 'Write a file'
+  })
   .help('h')
   .alias('h', 'help')
   .argv;
-console.log(`Parsing ${path.resolve(argv.f)}`);
+process.stderr.write(`Parsing ${path.resolve(argv.f)}`);
 
 const content = fs.readFileSync(argv.f, { encoding: 'utf8'} );
 
@@ -54,41 +61,92 @@ const makeW3CCompliant = (emscriptenIdl: string): string /*[compliantSource: str
 };
 // const [compliantSource, implementsStatements] = makeW3CCompliant(content);
 const compliantSource = makeW3CCompliant(content);
-// console.log(implementsStatements);
-const parsed = parse(compliantSource);
-console.log(parsed);
+// process.stderr.write(implementsStatements);
+const roots: WebIDL2.IDLRootType[] = parse(compliantSource);
+console.log(roots);
 
-function makeFactorialFunction() {
-  const functionName = ts.createIdentifier("factorial");
-  const paramName = ts.createIdentifier("n");
-  const parameter = ts.createParameter(
-    /*decorators*/ undefined,
-    /*modifiers*/ undefined,
-    /*dotDotDotToken*/ undefined,
-    paramName
-  );
+// const generateTypings = (roots: WebIDL2.IDLRootType[]): ts.Node /*ts.FunctionDeclaration*/ => {
+//   ts.createInterfaceDeclaration
+//   roots.forEach((root: WebIDL2.IDLRootType) => {
 
-  const condition = ts.createBinary(paramName, ts.SyntaxKind.LessThanEqualsToken, ts.createLiteral(1));
-  const ifBody = ts.createBlock([ts.createReturn(ts.createLiteral(1))], /*multiline*/ true);
+//   });
+  // return roots.reduce<ts.NodeArray<T>>((acc: ts.NodeArray<T>, root: WebIDL2.IDLRootType): ts.NodeArray<T> => {
+  //   return acc;
+  // }, [] as ts.NodeArray<T>);
+  // const functionName = ts.createIdentifier("factorial");
+  // const paramName = ts.createIdentifier("n");
+  // const parameter = ts.createParameter(
+  //   /*decorators*/ undefined,
+  //   /*modifiers*/ undefined,
+  //   /*dotDotDotToken*/ undefined,
+  //   paramName
+  // );
 
-  const decrementedArg = ts.createBinary(paramName, ts.SyntaxKind.MinusToken, ts.createLiteral(1));
-  const recurse = ts.createBinary(paramName, ts.SyntaxKind.AsteriskToken, ts.createCall(functionName, /*typeArgs*/ undefined, [decrementedArg]));
-  const statements = [ts.createIf(condition, ifBody), ts.createReturn(recurse)];
+  // const condition = ts.createBinary(paramName, ts.SyntaxKind.LessThanEqualsToken, ts.createLiteral(1));
+  // const ifBody = ts.createBlock([ts.createReturn(ts.createLiteral(1))], /*multiline*/ true);
 
-  return ts.createFunctionDeclaration(
-    /*decorators*/ undefined,
-    /*modifiers*/ [ts.createToken(ts.SyntaxKind.ExportKeyword)],
-    /*asteriskToken*/ undefined,
-    functionName,
-    /*typeParameters*/ undefined,
-    [parameter],
-    /*returnType*/ ts.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword),
-    ts.createBlock(statements, /*multiline*/ true)
-  );
-}
+  // const decrementedArg = ts.createBinary(paramName, ts.SyntaxKind.MinusToken, ts.createLiteral(1));
+  // const recurse = ts.createBinary(paramName, ts.SyntaxKind.AsteriskToken, ts.createCall(functionName, /*typeArgs*/ undefined, [decrementedArg]));
+  // const statements = [ts.createIf(condition, ifBody), ts.createReturn(recurse)];
 
-const resultFile = ts.createSourceFile("someFileName.ts", "", ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
-const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+  // return ts.createFunctionDeclaration(
+  //   /*decorators*/ undefined,
+  //   /*modifiers*/ [ts.createToken(ts.SyntaxKind.ExportKeyword)],
+  //   /*asteriskToken*/ undefined,
+  //   functionName,
+  //   /*typeParameters*/ undefined,
+  //   [parameter],
+  //   /*returnType*/ ts.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword),
+  //   ts.createBlock(statements, /*multiline*/ true)
+  // );
+// }
 
-const result = printer.printNode(ts.EmitHint.Unspecified, makeFactorialFunction(), resultFile);
-console.log(result);
+const compile = (options: ts.CompilerOptions): void => {
+  const host = ts.createCompilerHost(options);
+  host.readFile = () => '';
+  // host.writeFile = (fileName: string, contents: string, writeByteOrderMark: boolean, onError: (message: string) => void) =>
+  //   fs.writeFile(path.resolve('dist', fileName), contents, {
+  //     encoding: 'utf8'
+  //   }, (err: NodeJS.ErrnoException | null) => {
+  //     onError(err.message);
+  //   });
+  // const program = ts.createProgram([], options, host);
+  const program = ts.createProgram(['out.ts'], options, host);
+  program.emit(
+    /*targetSourceFile?: SourceFile*/ undefined,
+    /*writeFile?: WriteFileCallback*/ undefined,
+    /*cancellationToken?: CancellationToken*/ undefined,
+    /*emitOnlyDtsFiles?: boolean*/ undefined,
+    /*customTransformers?: CustomTransformers*/ {
+      before: [(context: ts.TransformationContext): ts.Transformer<ts.SourceFile> => (node: ts.SourceFile): ts.SourceFile => {
+        console.log(node);
+        console.log(context);
+        const sourceFile = ts.createSourceFile("someFileName.ts", "", ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
+        return sourceFile;
+      }]
+    });
+};
+
+compile({
+  // declaration: true,
+  // emitDeclarationOnly: true,
+});
+
+// const resultFile = ts.createSourceFile("someFileName.ts", "", ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
+// const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+// printer.printFile(resultFile);
+
+// const result = printer.printNode(ts.EmitHint.Unspecified, generateTypings(roots), resultFile);
+// process.stdout.write(result);
+// if (argv.o) {
+//   process.stderr.write(`Writing typings to ${path.resolve(argv.o)}`)
+//   fs.writeFile(argv.o, result, {
+//     encoding: 'utf8'
+//   }, (err: NodeJS.ErrnoException | null) => {
+//     if (err) {
+//       console.error(err);
+//       process.exit(1);
+//     }
+//     process.exit(0);
+//   });
+// }
