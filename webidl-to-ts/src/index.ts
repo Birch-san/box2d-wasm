4cdb7b027012ebfae8a1623589f4e749857041d9
Alex Birch <Birch-san@users.noreply.github.com>
2020-10-14 20:01:57 +0100

    tidy up

--- webidl-to-ts/src/index.ts
+++ webidl-to-ts/src/index.ts
@@ -7,20 +7,20 @@ const { parse } = WebIDL2;
 
 const argv = yargs(process.argv.slice(2))
   .usage('Usage: node --loader ts-node/esm --experimental-specifier-resolution=node --harmony -r source-map-support/register src/index.ts [options]')
-  .example('node --loader ts-node/esm --experimental-specifier-resolution=node --harmony -r source-map-support/register src/index.ts -f ../box2d-wasm/Box2D.idl', 'count the lines in the given file')
+  .example('node --loader ts-node/esm --experimental-specifier-resolution=node --harmony -r source-map-support/register src/index.ts -f ../box2d-wasm/Box2D.idl -o dist/Box2D.d.ts', 'count the lines in the given file')
   .option('f', {
     type: 'string',
     demandOption: true,
     alias: 'file',
     nargs: 1,
-    describe: 'Load a file'
+    describe: 'Read WebIDL input from a .idl file'
   })
   .option('o', {
     type: 'string',
     demandOption: false,
-    alias: 'file',
+    alias: 'output',
     nargs: 1,
-    describe: 'Write a file'
+    describe: 'Output Typescript declarations to a .d.ts file'
   })
   .help('h')
   .alias('h', 'help')
@@ -30,9 +30,8 @@ process.stderr.write(`Parsing ${path.resolve(argv.f)}`);
 const content = fs.readFileSync(argv.f, { encoding: 'utf8'} );
 
 type ImplementsStatement = [all: string, implementor: string, implemented: string];
-// type ImplementsStatements = { [implementor: string]: string };
 
-const makeW3CCompliant = (emscriptenIdl: string): string /*[compliantSource: string, implementsStatements: ImplementsStatements]*/ => {
+const makeW3CCompliant = (emscriptenIdl: string): string => {
   // https://github.com/osman-turan/ammo.js-typings#why-is-ammoidl-file-different-from-ammojs-repository
   // Emscripten uses a non-conforming WebIDL parser
   // whereas we use W3C's WebIDL parser
@@ -51,72 +50,39 @@ const makeW3CCompliant = (emscriptenIdl: string): string /*[compliantSource: str
   const needsToBeInheritedFromInterfaceMatcher = new RegExp(`^interface${whitespace.source}(${needsToBeInheritedFrom.join('|')})${whitespace.source}{$`, 'mg');
   const inheritedInterfacesAsMixins = implementsStatementsAsIncludesStatements.replace(needsToBeInheritedFromInterfaceMatcher, 'interface mixin $1 {');
 
-  // const implementsStatements = implementsStatementMatches.reduce<ImplementsStatements>(
-  //   (acc: ImplementsStatements, [, implementor, implemented]: ImplementsStatement): ImplementsStatements => {
-  //     acc[implementor] = implemented;
-  //     return acc;
-  //   }, {})
-  // return [implementsStatementsAsIncludesStatements, implementsStatements];
   return inheritedInterfacesAsMixins;
 };
-// const [compliantSource, implementsStatements] = makeW3CCompliant(content);
-// process.stderr.write(implementsStatements);
 
 const compliantSource = makeW3CCompliant(content);
 const roots: WebIDL2.IDLRootType[] = parse(compliantSource);
 console.log(roots);
 
-// const generateTypings = (roots: WebIDL2.IDLRootType[]): ts.Node /*ts.FunctionDeclaration*/ => {
-//   ts.createInterfaceDeclaration
-//   roots.forEach((root: WebIDL2.IDLRootType) => {
-
-//   });
-  // return roots.reduce<ts.NodeArray<T>>((acc: ts.NodeArray<T>, root: WebIDL2.IDLRootType): ts.NodeArray<T> => {
-  //   return acc;
-  // }, [] as ts.NodeArray<T>);
-  // const functionName = ts.createIdentifier("factorial");
-  // const paramName = ts.createIdentifier("n");
-  // const parameter = ts.createParameter(
-  //   /*decorators*/ undefined,
-  //   /*modifiers*/ undefined,
-  //   /*dotDotDotToken*/ undefined,
-  //   paramName
-  // );
-
-  // const condition = ts.createBinary(paramName, ts.SyntaxKind.LessThanEqualsToken, ts.createLiteral(1));
-  // const ifBody = ts.createBlock([ts.createReturn(ts.createLiteral(1))], /*multiline*/ true);
-
-  // const decrementedArg = ts.createBinary(paramName, ts.SyntaxKind.MinusToken, ts.createLiteral(1));
-  // const recurse = ts.createBinary(paramName, ts.SyntaxKind.AsteriskToken, ts.createCall(functionName, /*typeArgs*/ undefined, [decrementedArg]));
-  // const statements = [ts.createIf(condition, ifBody), ts.createReturn(recurse)];
-
-  // return ts.createFunctionDeclaration(
-  //   /*decorators*/ undefined,
-  //   /*modifiers*/ [ts.createToken(ts.SyntaxKind.ExportKeyword)],
-  //   /*asteriskToken*/ undefined,
-  //   functionName,
-  //   /*typeParameters*/ undefined,
-  //   [parameter],
-  //   /*returnType*/ ts.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword),
-  //   ts.createBlock(statements, /*multiline*/ true)
-  // );
-// }
-
 const compile = (options: ts.CompilerOptions): void => {
   const host = ts.createCompilerHost(options);
-  host.readFile = () => '';
-  host.writeFile = (fileName: string, contents: string) => {
-    console.log(fileName);
-    console.log(contents);
+  host.writeFile = (_fileName: string, contents: string, _writeByteOrderMark: boolean, onError: (message: string) => void) => {
+    // ignore provided filename; we've allowed user to optionally configure a filename of their choosing,
+    // unrelated to the placeholder name we gave to our empty source file
+    if (argv.o) {
+      process.stderr.write(`Writing typings to ${path.resolve(argv.o)}`);
+      if (!fs.existsSync(path.dirname(argv.o))) {
+        fs.mkdirSync(path.dirname(argv.o), { recursive: true });
+      }
+      fs.writeFile(argv.o, contents, {
+        encoding: 'utf8'
+      }, (err: NodeJS.ErrnoException | null) => {
+        if (err) {
+          process.exitCode = 1;
+          onError(err.message);
+        }
+      });
+    } else {
+      process.stderr.write(`Writing typings to stdout`);
+      process.stdout.write(contents);
+    }
   };
-  // host.writeFile = (fileName: string, contents: string, writeByteOrderMark: boolean, onError: (message: string) => void) =>
-  //   fs.writeFile(path.resolve('dist', fileName), contents, {
-  //     encoding: 'utf8'
-  //   }, (err: NodeJS.ErrnoException | null) => {
-  //     onError(err.message);
-  //   });
-  // const program = ts.createProgram([], options, host);
-  // const program = ts.createProgram([path.resolve('dist', 'out.ts')], options, host);
+  // the empty source file we'll use as input doesn't need to exist; we can just pretend to read its content
+  host.readFile = () => '';
+  // our initial source is an empty TypeScript file
   const program = ts.createProgram(['out.ts'], options, host);
   program.emit(
     /*targetSourceFile?: SourceFile*/ undefined,
@@ -128,31 +94,17 @@ const compile = (options: ts.CompilerOptions): void => {
         // context.enable
         return factory.updateSourceFile(
           rootNode,
-          /*ts.setTextRange(
-            factory.createNodeArray(*/[
-              factory.createInterfaceDeclaration(
-                /*decorators*/ undefined,
-                /*modifiers*/ [factory.createToken(ts.SyntaxKind.ExportKeyword)],
-                factory.createIdentifier('TestInterface'),
-                /*typeParameters*/ undefined,
-                /*heritageClauses*/ undefined,
-                /*members*/ []
-              )
-            ]/*),
-            *//*location*//*rootNode.statements
-          )*/,
+          [
+            factory.createInterfaceDeclaration(
+              /*decorators*/ undefined,
+              /*modifiers*/ [factory.createToken(ts.SyntaxKind.ExportKeyword)],
+              factory.createIdentifier('TestInterface'),
+              /*typeParameters*/ undefined,
+              /*heritageClauses*/ undefined,
+              /*members*/ []
+            )
+          ],
         /*isDeclarationFile*/true);
-        // const visit = (node: ts.Node): ts.Node => {
-        //   return ts.visitEachChild(node, visit, context);
-        // };
-        // return ts.visitNode(rootNode, visit);
-        // context.factory.statem
-
-        // context.factory.add
-        // context.factory.createInterfaceDeclaration().
-        // const sourceFile = ts.createSourceFile("someFileName.ts", "", ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
-        // return sourceFile;
-        // return rootNode;
       }]
     });
 };
@@ -161,22 +113,3 @@ compile({
   declaration: true,
   emitDeclarationOnly: true,
 });
-
-// const resultFile = ts.createSourceFile("someFileName.ts", "", ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
-// const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-// printer.printFile(resultFile);
-
-// const result = printer.printNode(ts.EmitHint.Unspecified, generateTypings(roots), resultFile);
-// process.stdout.write(result);
-// if (argv.o) {
-//   process.stderr.write(`Writing typings to ${path.resolve(argv.o)}`)
-//   fs.writeFile(argv.o, result, {
-//     encoding: 'utf8'
-//   }, (err: NodeJS.ErrnoException | null) => {
-//     if (err) {
-//       console.error(err);
-//       process.exit(1);
-//     }
-//     process.exit(0);
-//   });
-// }
