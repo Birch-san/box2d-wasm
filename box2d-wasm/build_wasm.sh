851e5cbe30abb83c92741c9e2f715fc4037bd1d6
Alex Birch <Birch-san@users.noreply.github.com>
2020-10-26 23:49:46 +0000

    dunno which flag I deleted that resulted in not having _malloc and _free any more, but let's ask for them explicitly

--- box2d-wasm/build_wasm.sh
+++ box2d-wasm/build_wasm.sh
@@ -11,7 +11,7 @@ if ! [[ "$PWD" -ef "$DIR/build" ]]; then
   exit 1
 fi
 
-LINK_OPTS='-s MODULARIZE=1 -s EXPORT_NAME="Box2D" -s EXPORT_BINDINGS=1 -s RESERVED_FUNCTION_POINTERS=20 --post-js box2d_glue.js --memory-init-file 0 -s NO_EXIT_RUNTIME=1 -s NO_FILESYSTEM=1 -s EXPORTED_RUNTIME_METHODS=[]'
+LINK_OPTS="-s MODULARIZE=1 -s EXPORT_NAME=Box2D -s EXPORT_BINDINGS=1 -s RESERVED_FUNCTION_POINTERS=20 --post-js box2d_glue.js --memory-init-file 0 -s NO_EXIT_RUNTIME=1 -s NO_FILESYSTEM=1 -s EXPORTED_RUNTIME_METHODS=[] -s EXPORTED_FUNCTIONS=['_malloc','_free']"
 
 
 # I decided to keep assertions, because in Box2D v2.4.1 an assertion (inertia must be positive)
