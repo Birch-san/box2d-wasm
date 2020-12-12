4f119ebaece3f62852d8a66c1078e63475e10083
Alex Birch <Birch-san@users.noreply.github.com>
2020-12-12 21:16:54 +0000

    destroy doesn't necessarily give us back all the memory of the object

--- integration-test-backend/src/measureHeap.ts
+++ integration-test-backend/src/measureHeap.ts
@@ -1,7 +1,9 @@
+export type HeapRemaining = () => number
+
 export const heapMeasurer = (
   _malloc: EmscriptenModule['_malloc'],
   _free: EmscriptenModule['_free']
-) =>
+): HeapRemaining =>
   (): number => {
     const ptrs: number[] = []
     let total = 0
@@ -26,8 +28,25 @@ export const heapMeasurer = (
     } catch (e) {
       console.error(e)
     }
-    for (const s in counts) {
-      console.log(`${counts[s]} of size ${s}`)
-    }
+    // for (const s in counts) {
+    //   console.log(`${counts[s]} of size ${s}`)
+    // }
     return total
   }
+
+export class HeapTracker {
+  private readonly initial: number
+  private current: number
+  constructor (
+    private readonly heapRemaining: HeapRemaining
+  ) {
+    this.current = this.initial = heapRemaining()
+  }
+
+  track (event: string): void {
+    const current: number = this.heapRemaining()
+    const delta: number = current - this.current
+    this.current = current
+    console.log(`${Math.sign(delta) === 1 ? '+' : ''}${delta}: ${event}`)
+  }
+}
--- integration-test-backend/src/memoryInvestigation.ts
+++ integration-test-backend/src/memoryInvestigation.ts
@@ -1,1 +1,63 @@
 // note: this is only gonna work if Box2D is compiled using ALLOW_MEMORY_GROWTH=0
+import Box2DFactory from 'box2d-wasm'
+import { heapMeasurer, HeapTracker, HeapRemaining } from './measureHeap'
+// import { assertFloatEqual } from './assertFloatEqual'
+
+const { b2BodyDef, b2_dynamicBody, b2PolygonShape, b2Vec2, b2World, _malloc, _free, destroy }: typeof Box2D & EmscriptenModule = await Box2DFactory({
+  printErr: (str: string) => {
+    if (str !== 'OOM') {
+      console.warn(str)
+    }
+  }
+})
+
+// console.error = () => {}
+// console.warn = () => {}
+// console.debug = () => {}
+const measureHeap: HeapRemaining = heapMeasurer(_malloc, _free)
+const tracker = new HeapTracker(measureHeap)
+
+const gravity = new b2Vec2(0, 10)
+tracker.track('allocated gravity')
+const world = new b2World(gravity)
+tracker.track('allocated world')
+destroy(gravity)
+tracker.track('freed gravity')
+
+// const sideLengthMetres = 1
+// const square = new b2PolygonShape()
+// square.SetAsBox(sideLengthMetres / 2, sideLengthMetres / 2)
+
+// const zero = new b2Vec2(0, 0)
+
+// const bd = new b2BodyDef()
+// bd.set_type(b2_dynamicBody)
+// bd.set_position(zero)
+
+// const body = world.CreateBody(bd)
+// body.CreateFixture(square, 1)
+// body.SetTransform(zero, 0)
+// body.SetLinearVelocity(zero)
+// body.SetAwake(true)
+// body.SetEnabled(true)
+
+// const timeStepMillis = 1 / 60
+// const velocityIterations = 1
+// const positionIterations = 1
+// const floatCompareTolerance = 0.01
+
+// const iterations = 6
+// for (let i = 0; i < iterations; i++) {
+//   const timeElapsedMillis = timeStepMillis * i
+//   {
+//     const { y } = body.GetLinearVelocity()
+//     assertFloatEqual(y, gravity.y * timeElapsedMillis, floatCompareTolerance)
+//     {
+//       const { y } = body.GetPosition()
+//       assertFloatEqual(y, 0.5 * gravity.y * timeElapsedMillis ** 2, floatCompareTolerance)
+//     }
+//   }
+//   world.Step(timeStepMillis, velocityIterations, positionIterations)
+// }
+
+// console.log(`👍 Ran ${iterations} iterations of a falling body. Body had the expected position on each iteration.`)
