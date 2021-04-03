d66d1d9cf0c807552d2509784662d321416c0d0c
Alex Birch <Birch-san@users.noreply.github.com>
2021-04-03 18:46:51 +0100

    build WASM and asm.js

--- box2d-wasm/build_wasm.sh
+++ box2d-wasm/build_wasm.sh
@@ -25,6 +25,7 @@ EMCC_OPTS=(
   -s SUPPORT_LONGJMP=0
   -s EXPORTED_FUNCTIONS=_malloc,_free
   -s ALLOW_MEMORY_GROWTH=1
+  -s WASM=2
   )
 RELEASE_OPTS=(-O3)
 
@@ -85,20 +86,42 @@ mkdir -p "$UMD_DIR" "$ES_DIR"
 
 LINK_OPTS=(--post-link "$BARE_WASM" --post-js "$DIR/glue_stub.js" ${EMCC_OPTS[@]})
 
-if [ "$SKIP_UMD_BUILD" = "1" ]; then
-  >&2 echo -e "${Green}Skipped UMD build because we gotta go fast${NC}"
-else
-  UMD_FILE="$UMD_DIR/$BASENAME.js"
-  >&2 echo -e "${Blue}Building UMD module, $UMD_FILE${NC}"
-  set -x
-  emcc "${LINK_OPTS[@]}" -o "$UMD_FILE"
-  { set +x; } 2>&-
-  >&2 echo -e "${Green}Successfully built $UMD_FILE${NC}\n"
-fi
-
 ES_FILE="$ES_DIR/$BASENAME.js"
->&2 echo -e "${Blue}Building ES module, $ES_FILE${NC}"
+>&2 echo -e "${Blue}Building ES module, $ES_DIR/$BASENAME.{js,wasm{,.js}}${NC}"
 set -x
 emcc "${LINK_OPTS[@]}" -s EXPORT_ES6=1 -o "$ES_FILE"
 { set +x; } 2>&-
->&2 echo -e "${Green}Successfully built $ES_FILE${NC}"
+>&2 echo -e "${Green}Successfully built $ES_DIR/$BASENAME.{js,wasm{,.js}}${NC}\n"
+
+UMD_FILE="$UMD_DIR/$BASENAME.js"
+if [ "$BUILD_UMD_FROM_SCRATCH" = "1" ]; then
+  >&2 echo -e "${Blue}Building UMD module, $UMD_DIR/$BASENAME.{js,wasm{,.js}} from scratch${NC}"
+  set -x
+  emcc "${LINK_OPTS[@]}" -o "$UMD_FILE"
+  { set +x; } 2>&-
+else
+  >&2 echo -e "${Blue}Building UMD module, $UMD_DIR/$BASENAME.{js,wasm{,.js}} by replacing header & footer of ES module${NC}"
+  escape_for_sed_replace () {
+    echo "$1" | sed -e 's/&/\\\&/g' -e '$!s/$/\\n/' | tr -d '\n'
+  }
+
+  ES6_HEADER='  var _scriptDir = import.meta.url;'
+  UMD_HEADER="  var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
+  if (typeof __filename !== 'undefined') _scriptDir = _scriptDir || __filename;"
+  UMD_HEADER_ESCAPED=`escape_for_sed_replace "$UMD_HEADER"`
+
+  ES6_FOOTER='export default Box2D;'
+  UMD_FOOTER="if (typeof exports === 'object' && typeof module === 'object')
+      module.exports = Box2D;
+    else if (typeof define === 'function' && define['amd'])
+      define([], function() { return Box2D; });
+    else if (typeof exports === 'object')
+      exports['Box2D'] = Box2D;
+    
+"
+  UMD_FOOTER_ESCAPED=`escape_for_sed_replace "$UMD_FOOTER"`
+
+  sed -e "s/^$ES6_HEADER$/$UMD_HEADER_ESCAPED/" -e "s/^$ES6_FOOTER$/$UMD_FOOTER_ESCAPED/" "$ES_FILE" > "$UMD_FILE"
+  cp "$ES_DIR/$BASENAME.wasm"{,.js} "$UMD_DIR"
+fi
+>&2 echo -e "${Green}Successfully built $UMD_DIR/$BASENAME.{js,wasm{,.js}}${NC}\n"
