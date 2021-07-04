0bbb20640cf52b85ecfd9ffbb4b77f85a2f92e24
Alex Birch <Birch-san@users.noreply.github.com>
2021-07-04 20:04:51 +0100

    UMD entrypoint was actually a CJS entrypoint. keep it around, but write a new UMD entrypoint which works with both Web and Node.

--- box2d-wasm/entry/cjs/.eslintrc.cjs
+++ box2d-wasm/entry/cjs/.eslintrc.cjs
@@ -3,9 +3,7 @@ module.exports = {
     ecmaVersion: 2019,
   },
   env: {
-    browser: true,
-    es6: true,
-    node: false,
-    commonjs: false
+    commonjs: true,
+    es6: true
   }
 }
--- box2d-wasm/entry/umd/entry.js
+++ box2d-wasm/entry/umd/entry.js
@@ -1,11 +1,37 @@
-const { simd } = require('wasm-feature-detect');
-
-module.exports = async (...args) => {
-  const hasSIMD = await simd();
-  const Box2DFactory = (
-    hasSIMD
-      ? require('../../build/flavour/simd/umd/Box2D.simd.js')
-      : require('../../build/flavour/standard/umd/Box2D.js')
-  );
-  return Box2DFactory(...args);
-};
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['wasm-feature-detect', 'require'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // NodeJS
+    module.exports = factory(require('wasm-feature-detect'), require);
+  } else {
+    // Browser globals (root is window)
+    const loadModule = (path) => new Promise((resolve, reject) => {
+      const tag = root.document.createElement("script");
+      tag.onload = () => {
+        resolve(root.Box2D);
+        return false;
+      };
+      tag.onerror = ({ target: { src } }) => {
+        reject(new Error(`Failed to load '${src}'`));
+        return false;
+      };
+      tag.src = path;
+      root.document.getElementsByTagName("head")[0].appendChild(tag);
+    });
+    root.Box2D = factory(root.wasmFeatureDetect, loadModule, true);
+  }
+}(typeof self !== 'undefined' ? self : this, ({ simd }, require, requireIsAsync) =>
+  async (...args) => {
+    const hasSIMD = await simd();
+    const module = (
+      hasSIMD
+        ? require('../../build/flavour/simd/umd/Box2D.simd.js')
+        : require('../../build/flavour/standard/umd/Box2D.js')
+    );
+    const Box2DFactory = requireIsAsync
+      ? await module
+      : module;
+    return Box2DFactory(...args);
+}));
