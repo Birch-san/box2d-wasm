6baf67c111b2bf20355ab16707d1a3f6c70a3615
Alex Birch <Birch-san@users.noreply.github.com>
2021-07-04 17:40:54 +0100

    backend integration test working

--- box2d-wasm/entry/umd/entry.js
+++ box2d-wasm/entry/umd/entry.js
@@ -1,12 +1,11 @@
-// this file will be copied into the build/es folder
 const { simd } = require('wasm-feature-detect');
 
-module.exports = (async () => {
+module.exports = async (...args) => {
   const hasSIMD = await simd();
-  const module = (
+  const Box2DFactory = (
     hasSIMD
       ? require('../../build/flavour/simd/umd/Box2D.simd.js')
       : require('../../build/flavour/standard/umd/Box2D.js')
   );
-  return module;
-})();
+  return Box2DFactory(...args);
+};
